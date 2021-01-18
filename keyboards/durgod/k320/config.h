06b43ccfd0180ea9c757a9bd92a9f0136fe7366f
Don Kjer <don.kjer@gmail.com>
2021-01-17 16:35:45 -0800

    Updating K320 vendor/product ids

--- keyboards/durgod/k320/config.h
+++ keyboards/durgod/k320/config.h
@@ -20,9 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0320
-#define DEVICE_VER      0x0003
+#define VENDOR_ID       0xD60D
+#define PRODUCT_ID      0x3200
+#define DEVICE_VER      0x0001
 #define MANUFACTURER    Hoksi Technology
 #define PRODUCT         DURGOD Taurus K320 (QMK)
 
