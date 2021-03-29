172719c2043ce86131cfa3a18823bece2167c515
Don Kjer <don.kjer@gmail.com>
2021-03-29 03:30:57 -0700

    Disabling spacebar / fn1

--- keyboards/durgod/venus/keymaps/dkjer/keymap.c
+++ keyboards/durgod/venus/keymaps/dkjer/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤
    * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │  ↑Shift  │
    * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
-   * │Ctrl│Alt │GUI │       Space/Fn1        │Ctrl│←Fn1│↓Fn2│→Ctl│
+   * │Ctrl│Alt │GUI │         Space          │Ctrl│←Fn1│↓Fn2│→Ctl│
    * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
    */
   [0] = LAYOUT_60_ansi(
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
       KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            RSFT_T(KC_UP),
-      KC_LCTL,  KC_LALT,  KC_LGUI,                                LT(1, KC_SPC),                          KC_RCTL,LT(1,KC_LEFT),LT(2,KC_DOWN),RCTL_T(KC_RIGHT)
+      KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RCTL,LT(1,KC_LEFT),LT(2,KC_DOWN),RCTL_T(KC_RIGHT)
   ),
   /* Layer 1
    * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
